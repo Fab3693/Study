3285d02a1f1c919c41b6edb3cc28942d9b1ac446
Alex <Aleksandr-aksinin98@mail.ru>
2024-09-07 12:51:17 +0700

    Initial commit

---
+++ src/Study/Kek.java
@@ -0,0 +1,16 @@
+package Study;
+
+import java.util.concurrent.TimeUnit;
+
+public class Kek {
+    public static void main(String[] args) {
+        taskForCondition();
+    }
+
+    public static void taskForCondition() {
+        int number = 15;
+        boolean firstCondition = number < 15 && number > 25 && (number += 15) < 25;
+        boolean secondCondition = (number | 5) > 12 || (number = number + 2) > 40;
+        System.out.println(number);
+    }
+}
---
+++ src/Study/Main.java
@@ -0,0 +1,14 @@
+package Study;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+import static java.lang.Math.toRadians;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Заработаю ли я себе на BMW?");
+        System.out.println(MagicBall.getPrediction());
+    }
+
+}
